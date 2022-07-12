c4f4965bac62dadfba6acced1233b57341f1ba87
kynta-ita <104131131+kynta-ita@users.noreply.github.com>
2022-07-12 17:50:06 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+    $host = "localhost";
+    $user = "root";
+    $password = "";
+    $db = "nilaiMahasiswa";
+
+    $conn = mysqli_connect($host,$user, $password,$db);
+
+    if (mysqli_connect_error()){
+        echo "koneksi gagal". mysqli_connect_error();
+    }
+?>
